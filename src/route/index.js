0fb4a0e30676350104f693f4e191f120e6001bbb
Turaiev Kyrylo <38906421+2rv@users.noreply.github.com>
2023-04-20 15:35:14 +0300

    Update index.js

--- src/route/index.js
+++ src/route/index.js
@@ -25,11 +25,11 @@ router.get('/', function (req, res) {
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/template-1', function (req, res) {
+router.get('/template-2', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('template-1', {
+  res.render('template-2', {
     page: {
       title: 'Example Page',
     },
